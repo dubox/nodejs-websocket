78ef8de1a988b6fe05e1d7b1f759b189bfa2e053
dubox <dubo_x@qq.com>
2025-02-27 18:21:57 +0800

    add http

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const WebSocket = require('ws');
 const WebSocketServer = WebSocket.Server;
 const port = 3000
-const wss = new WebSocketServer({ port });
+
+const http = require('http');
+
+// 创建HTTP服务器
+const server = http.createServer((req, res) => {
+  res.writeHead(404);
+    res.end('404');
+});
+
+// 创建WebSocket服务器并绑定到HTTP服务器
+const wss = new WebSocket.Server({ server });
+// const wss = new WebSocketServer({ port });
 const HEARTBEAT_INTERVAL = 30000; // 30秒发送一次Ping
 wss.on('connection', (ws) => {
     const clinetId = uuidv4();
@@ -39,7 +50,10 @@ wss.on('error', (error) => {
     console.error(error);
 });
 
-  
+  // 启动服务
+server.listen(port, () => {
+  console.log('服务器运行在 http://localhost:8080');
+});
 
 
 // private methods
